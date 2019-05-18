60115b33cdb8394b90f8f7a7116eb53cae2ade6a
Thanh <nhutthanh340@gmail.com>
2019-05-18 21:43:44 +0700

    vietsub index

--- Source/WebChiaSe/routes/index.js
+++ Source/WebChiaSe/routes/index.js
@@ -6,6 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index.hbs', { title: 'Express' });
 });
 
+router.get('/about-us', function(req, res, next) {
+  res.render('about-us.hbs', { title: 'Express' });
+});
 
+router.get('/blog', function(req, res, next) {
+  res.render('blog.hbs', { title: 'Express' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact.hbs', { title: 'Express' });
+});
+router.get('/course-details', function(req, res, next) {
+  res.render('course-details.hbs', { title: 'Express' });
+});
+router.get('/courses', function(req, res, next) {
+  res.render('courses.hbs', { title: 'Express' });
+});
+
+router.get('/elements', function(req, res, next) {
+  res.render('elements.hbs', { title: 'Express' });
+});
+router.get('/index', function(req, res, next) {
+  res.render('index.hbs', { title: 'Express' });
+});
 
 module.exports = router;
