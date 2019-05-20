70767c8a22a8c68b4e3b6c4dce618f87a600759f
Thanh <nhutthanh340@gmail.com>
2019-05-20 11:38:49 +0700

    login

--- Source/WebChiaSe/routes/index.js
+++ Source/WebChiaSe/routes/index.js
@@ -9,7 +9,24 @@ router.get('/', function(req, res, next) {
 router.get('/single-blog', function(req, res, next) {
   res.render('single-blog.hbs', { title: 'Express' });
 });
+router.get('/blog', function(req, res, next) {
+  res.render('blog.hbs', { title: 'Express' });
+});
+router.get('/courses', function(req, res, next) {
+  res.render('courses.hbs', { title: 'Express' });
+});
+router.get('/elements', function(req, res, next) {
+  res.render('elements.hbs', { title: 'Express' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact.hbs', { title: 'Express' });
+});
+router.get('/course-details', function(req, res, next) {
+  res.render('course-details.hbs', { title: 'Express' });
+});
 
-
+router.get('/login', function(req, res, next) {
+  res.render('login.hbs', { title: 'Express' });
+});
 
 module.exports = router;
