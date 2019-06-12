3a42df1a382848eceb10d9837b2574aeefb6acdf
luongngocvu <luongngocvu168@gmail.com>
2019-06-12 08:51:44 +0700

    edit layout show blog/ detail blog + search blog

--- Cài đặt lần 1/Source code/Thành viên/controllers/BaiDangController.js
+++ Cài đặt lần 1/Source code/Thành viên/controllers/BaiDangController.js
@@ -8,16 +8,21 @@ exports.ShowPostByType = async function (req, res) {
     const idTypePost = req.params.id;
     const post = await BaiDang.readByTypePost(idTypePost);
     const type = await TheLoai.readAll();
-    console.log(post);
     res.render('baidang', {user: req.user, type: type, post: post});
 };
 
 exports.DetailBlog = async function (req, res) {
-    // const baidang = await BaiDang.read(req.params.id);
     const Content = await fs.readFileSync('NoiDungBaiViet/1.html');
     const type = await TheLoai.readAll();
     const idPost = req.params.id;
-    const detail_1 = await BaiDang.readDetailPostById(idPost);
+    const detail_1 = await BaiDang.readDetailBlogById(idPost);
     const detail = JSON.parse(JSON.stringify(detail_1[0]));
     res.render('single-blog', {user: req.user, type, detail, Content});
+};
+
+exports.searchBlog = async function (req, res) {
+    const nameSearch = req.body.nameSearch;
+    const blog = await BaiDang.searchByName(nameSearch);
+    const type = await TheLoai.readAll();
+    res.render('baidang', {user: req.user, type: type, post: blog});
 };
--- Cài đặt lần 1/Source code/Thành viên/models/BaiDang.js
+++ Cài đặt lần 1/Source code/Thành viên/models/BaiDang.js
@@ -28,8 +28,14 @@ exports.readByTypePost = async function (id) {
     return result[0];
 };
 
-exports.readDetailPostById = async function (id) {
-    const sql = 'select TL.tentheloai, BD.id, BD.ngaydang, BV.tenbaiviet, BV.noidung from theloaibaiviet TL, baidang BD, baiviet BV where BV.id=BD.idbaiviet and TL.id=BV.idtheloaibaiviet and BD.id = ?';
+exports.readDetailBlogById = async function (id) {
+    const sql = 'select BV.tomtat, ND.hoten, TL.tentheloai, BD.id, BD.ngaydang, BV.tenbaiviet, BV.noidung from nguoidung ND, theloaibaiviet TL, baidang BD, baiviet BV where ND.id=BV.idnguoigui and BV.id=BD.idbaiviet and TL.id=BV.idtheloaibaiviet and BD.id = ?';
     const result = await Connection.promise().query(sql, id);
+    return result[0];
+};
+
+exports.searchByName = async function (name) {
+    const sql = 'select TL.tentheloai, BD.id, BD.ngaydang, BV.tenbaiviet, BV.tomtat from theloaibaiviet TL, baidang BD, baiviet BV where BV.id=BD.idbaiviet and TL.id=BV.idtheloaibaiviet and BV.tenbaiviet like ' + Connection.escape('%' + name + '%');
+    const result = await Connection.promise().query(sql);
     return result[0];
 };
